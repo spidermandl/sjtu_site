9cca15df79c1ff62fd4558d2266f935c6315abfa
Desmond Duan <Desmond@Desmonds-MacBook-Pro.local>
2017-04-21 15:38:10 +0800

    add news content

---
+++ application/views/article/1023.php
@@ -0,0 +1,37 @@
+<div class="pane-content">
+    <div class="views_view view view-node-body-with-title view-id-node_body_with_title view-display-id-panel_pane_1 view-dom-id-1">
+        <div class="view-content">
+            <div class="first last odd">
+                <h2>图灵奖得主Ivan Sutherland教授到访并作学术报告</h2>
+
+				<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2016年2月22日下午，1988年图灵奖得主Ivan Sutherland教授及其夫人Marly Roncken教授莅临，在200号报告厅为电院师生作了题为“Understanding Self-timed Circuits”的学术报告。报告分上下两场，上半场于22日下午进行；下半场于23日上午进行。近200名学生前来聆听了此次报告，现场座无虚席。
+				</p>
+				<br/>
+ 				<p>
+					<img src="../img/1023/1.jpg" style="height:70% !important;width:70% !important;" />
+				</p>
+				<br/>
+
+				<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;报告中，Sutherland教授和Roncken教授介绍了自计时系统的相关概念，以及如何设计、验证和测试自计时系统。Sutherland教授还在现场与学生进行游戏互动，以生动的形式和幽默的语言帮助学生们理解报告内容，赢得了现场师生的热烈响应。
+				</p>
+				<br/>
+ 				<p>
+					<img src="../img/1023/1.jpg" style="height:70% !important;width:70% !important;" />
+				</p>
+				<br/>
+
+				<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ivan Sutherland教授于1988年荣获图灵奖，被称作计算机图形学之父和虚拟现实之父。1963年，他在其麻省理工学院的博士论文中提出了三维交互式图形系统，并成功开发了著名的“画板”（SketchPad）系统，它是有史以来第一个交互式绘图系统，也是交互式电脑绘图的开端。人们后来在此基础上相继开发了CAD和CAM，它们被称为20世纪下半叶最杰出的工程技术成就之一。1965年，他发表了一篇名为“终极的显示”的论文，描述的就是我们现在熟悉的“虚拟现实”（Virtual Reality）。早在虚拟现实技术研究的初期，Sutherland教授就在其“达摩克利斯之剑”系统中实现了三维立体显示。增强现实系统是指在真实环境之上提供信息性和娱乐性的覆盖，它是Sutherland教授在进行有关头戴显示器的研究中引入的。（引自http://www.ituring.com.cn/article/127792）
+				</p>
+				<br/>
+ 				<p>
+					<img src="../img/1023/1.jpg" style="height:70% !important;width:70% !important;" />
+				</p>
+				<br/>
+
+             </div>
+        </div>
+    </div>
+</div>
